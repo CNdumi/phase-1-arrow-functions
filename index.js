6100a1c88dc30f884f2a0e5838ca26f2e41f3d72
CNdumi <carolinendumi12@gmail.com>
2023-06-14 12:15:01 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function divide() {
-        return 2000/100;
-    } 
-
-const square = (x) => x * x
-
-const add = (x, y) => x + y
+    return 2000 / 100;
+  }
+  
+  let x = 2;
+  const square = (x) => x * x;
+  
+  let a = 3;
+  let b = 4;
+  const add = (x, y) => x + y;
+  
+  console.log(divide()); 
+  
+  console.log(square(x)); 
+  
+  console.log(add(a, b)); 
+  
